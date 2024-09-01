d592c30cc3a84d676cd487b4f9abada21668f3ad
Amir Tavakolian <rezatva@gmail.com>
2024-09-01 16:15:56 +0330

    a simple response in json

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/response', function () {
+
+    return response()->json([
+        "message" => "successfully done",
+        "data" => [
+            "id" => 1,
+            "first-name" => "Amir",
+            "last-name" => "Tavakolian"
+        ]
+    ], 200);
+});
+
+
+// first parameter of json is our response's body
+
+// seconde one is our response's status code ==> by default its 200
+
+// 3th one is our response's header

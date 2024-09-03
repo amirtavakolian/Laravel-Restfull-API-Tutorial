14a643c928d3cf4a21c1f152ddf8707ed0fe1586
Amir Tavakolian <rezatva@gmail.com>
2024-09-03 09:42:56 +0330

    change the message of model not found (404 not found) exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -10,7 +12,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of exception types with their corresponding custom log levels.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
+     * @var array<class-string<Throwable>, \Psr\Log\LogLevel::*>
      */
     protected $levels = [
         //
@@ -19,7 +21,7 @@ class Handler extends ExceptionHandler
     /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
@@ -44,5 +46,28 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $e)
+    {
+        // we want to return json format response for 404 not found:
+
+
+        // you can use below code to get 404 in html for debugging environment
+        // because it has more info about the error. specially info about the database :D
+        // but for production mode, just return a simple "recorde not found" message :P and make app.debug false
+        if (config('app.debug')) {
+            return parent::render($request, $e);
+        }
+
+
+        if ($e instanceof ModelNotFoundException) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
+        }
+        // you can use try catch or above method for handling exceptions :D
     }
 }

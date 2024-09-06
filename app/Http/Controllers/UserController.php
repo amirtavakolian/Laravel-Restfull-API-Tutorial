321f17246e9d2a138a42402c6ab392f0da8a544d
Amir Tavakolian <rezatva@gmail.com>
2024-09-06 10:27:25 +0330

    install sanctum + add builder pattern + facade

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    
+    // check laravel's documentation for installing sanctum
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Models\User;
+use App\Services\RestApi\ApiResponseBuilder;
+use App\Services\RestApi\ApiResponseFacade;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+
+    $users = User::all();
+
+    return (new ApiResponseBuilder())
+        ->withMessage('list of users')
+        ->withData($users)
+        ->withStatus(200)
+        ->build()
+        ->response();
+
+
+    // use facade so there is no need to new ApiResponseBuilder
+
+    return ApiResponseFacade::withMessage('list of users')
+        ->withData($users)
+        ->build()
+        ->response();
+});

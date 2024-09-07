2dbb96435f4625f71df2314e41c86810b298865d
Amir Tavakolian <rezatva@gmail.com>
2024-09-07 16:51:11 +0330

    test login with token + auth:sanctum middleware

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
 Route::post('/users/login', [UserController::class, 'login']);
+
+
+Route::get('/test-token', function () {
+
+    return ["message" => "You are login"];
+})->middleware('auth:sanctum');
+
+// don't forget to put authorizatin header & the token you have + Accept: application/json header
